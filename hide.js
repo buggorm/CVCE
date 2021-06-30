5278710f986d7f1fdd3b8d12c82f68031bc3c44d
buggorm <83905549+buggorm@users.noreply.github.com>
2021-06-30 19:20:32 +0200

    Some changes

--- hide.js
+++ hide.js
@@ -4,14 +4,16 @@ function hide(elements) {
 	})
 }
 
+function query(selector) {
+	return Object.values(document.querySelectorAll(selector))
+}
+
+const bc = window.location.pathname.replace(/\//g, '')
+
 setInterval(() => {
-	let notices = Object.values(document.querySelectorAll('.dm-adjust'))
-		.filter(e => !e.getAttribute('data-nick'))
-	let links = Object.values(document.querySelectorAll('a'))
-		.filter(e => e.href.includes('external') || e.href.includes('campaign'))
-	let spam = Object.values(document.querySelectorAll('.dm-adjust'))
-		.filter(e => e.getAttribute('data-nick'))
-		.filter(e => e.innerText.includes('****') || e.innerText.includes('----'))
+	let notices = query('.dm-adjust').filter(e => !e.getAttribute('data-nick'))
+	let links = query('a').filter(e => e.href.includes('external') || e.href.includes('campaign'))
+	let spam = query('.dm-adjust').filter(e => e.getAttribute('data-nick')).filter(e => e.innerText.startsWith(bc) && (e.innerText.includes('****') || e.innerText.includes('----')))
 
 	hide(notices.concat(links).concat(spam))
-}, 1000)
+}, 500)
